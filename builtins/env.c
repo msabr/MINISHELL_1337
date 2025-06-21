6ac4675344cf330d668ec2411c88fe637c2de84e
Mohamed Sabr <msabr@e3r8p2.1337.ma>
2025-06-21 18:34:17 +0100

    fix env

--- builtins/env.c
+++ builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: msabr <msabr@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/14 14:52:50 by msabr             #+#    #+#             */
-/*   Updated: 2025/06/14 15:22:07 by msabr            ###   ########.fr       */
+/*   Updated: 2025/06/15 19:15:02 by msabr            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,22 @@
 void env_function(t_env *env_list)
 {
     t_env *current = env_list;
+    char *result;
 
     while (current)
     {
         if (current->value)
         {
-            ft_putstr_fd(current->key, STDOUT_FILENO);
-            ft_putchar_fd('=', STDOUT_FILENO);
-            ft_putstr_fd(current->value, STDOUT_FILENO);
+            result = ft_strjoin(current->key, "=");
+            result = ft_strjoin(result, current->value);
+            ft_putendl_fd(result, STDOUT_FILENO);
+            free(result);
+        
         }
         else
         {
-            ft_putstr_fd(current->key, STDOUT_FILENO);
+            ft_putendl_fd(current->key, STDOUT_FILENO);
         }
-        ft_putchar_fd('\n', STDOUT_FILENO);
         current = current->next;
     }
 }
