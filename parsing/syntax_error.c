b6c5dc750e782cb7adeb3154f0b85dae69c87c43
Khalid <khalidabouelfaraj371@gmail.com>
2025-07-04 19:06:01 +0100

    error syntaxe

--- parsing/syntax_error.c
+++ parsing/syntax_error.c
@@ -1,95 +1,98 @@
 #include "../minishell.h"
-#include <unistd.h>
 
-/* Affiche le message d'erreur bash-like et set status */
-void	syntax_error(const char *token)
+void	error_syntax(const char *token)
 {
-	ft_putstr_fd("minishell: syntax error near unexpected token `", STDERR_FILENO);
-	if (token && token[0])
-		ft_putstr_fd(token, STDERR_FILENO);
+	ft_putstr_fd("minishell: syntax error near unexpected token `", 2);
+	if (token && *token)
+		ft_putstr_fd(token, 2);
 	else
-		ft_putstr_fd("newline", STDERR_FILENO);
-	ft_putstr_fd("'\n", STDERR_FILENO);
+		ft_putstr_fd("newline", 2);
+	ft_putstr_fd("'\n", 2);
 	ft_set_status(258);
 }
 
-int	check_quotes(const char *input)
+int	check_unclosed_quote(const char *input)
 {
-	int	sq = 0, dq = 0;
-	size_t i = 0;
+	int		i;
+	char	quote;
+
+	i = 0;
+	quote = 0;
 	while (input && input[i])
 	{
-		if (input[i] == '\'' && dq % 2 == 0)
-			sq++;
-		else if (input[i] == '"' && sq % 2 == 0)
-			dq++;
+		if (!quote && (input[i] == '\'' || input[i] == '"'))
+			quote = input[i];
+		else if (quote && input[i] == quote)
+			quote = 0;
 		i++;
 	}
-	return (sq % 2 == 0 && dq % 2 == 0);
+	if (quote)
+	{
+		ft_set_status(258);
+		return (1);
+	}
+	return (0);
 }
 
-static int	is_redir(t_token_type t)
+const char	*token_repr(t_token *tok)
+{
+	if (!tok || tok->type == TOKEN_EOF)
+		return ("newline");
+	if (tok->type == TOKEN_PIPE)
+		return ("|");
+	if (tok->type == TOKEN_REDIR_IN)
+		return ("<");
+	if (tok->type == TOKEN_REDIR_OUT)
+		return (">");
+	if (tok->type == TOKEN_REDIR_APPEND)
+		return (">>");
+	if (tok->type == TOKEN_HEREDOC)
+		return ("<<");
+	if (tok->type == TOKEN_WORD || tok->type == TOKEN_VARIABLE)
+		return (tok->value);
+	return (tok->value ? tok->value : "");
+}
+
+int	is_redir(t_token_type t)
 {
 	return (t == TOKEN_REDIR_IN || t == TOKEN_REDIR_OUT
 		|| t == TOKEN_REDIR_APPEND || t == TOKEN_HEREDOC);
 }
 
-/* Nom lisible pour le token (pour l'affichage de l'erreur) */
-static const char *token_repr(t_token *tok)
-{
-	if (!tok || tok->type == TOKEN_EOF)
-		return "newline";
-	if (tok->type == TOKEN_PIPE)
-		return "|";
-	if (tok->type == TOKEN_REDIR_IN)
-		return "<";
-	if (tok->type == TOKEN_REDIR_OUT)
-		return ">";
-	if (tok->type == TOKEN_REDIR_APPEND)
-		return ">>";
-	if (tok->type == TOKEN_HEREDOC)
-		return "<<";
-	if (tok->type == TOKEN_WORD || tok->type == TOKEN_VARIABLE)
-		return tok->value;
-	if (tok->type == TOKEN_SQUOTE)
-		return "'";
-	if (tok->type == TOKEN_DQUOTE)
-		return "\"";
-	return tok->value ? tok->value : "";
-}
-
-/* Handler principal */
 int	check_syntax_errors(t_token *tokens, const char *input)
 {
-	t_token	*curr = tokens;
-	if (!check_quotes(input))
-		return (syntax_error("newline"), 1);
-	if (is_redir(curr->type) && curr->next && is_redir(curr->next->type))
-    	return (syntax_error(token_repr(curr->next)), 1);
+	t_token	*curr;
+
+	if (!input || !*input)
+	{
+		ft_set_status(0);
+		return (1); // Ne pas continuer
+	}
+	if (check_unclosed_quote(input))
+		return (1);
+	curr = tokens;
+	if (curr && (curr->type == TOKEN_PIPE || is_redir(curr->type)))
+		return (error_syntax(token_repr(curr)), 1);
 	while (curr && curr->type != TOKEN_EOF)
 	{
-		if (curr->type == TOKEN_PIPE && curr->next && curr->next->type == TOKEN_PIPE)
-			return (syntax_error("||"), 1);
-		if (curr->type == TOKEN_WORD && curr->value && curr->value[0] == '&'
-			&& curr->value[1] == '&')
-			return (syntax_error("&&"), 1);
-
-		if (is_redir(curr->type) && curr->next && is_redir(curr->next->type))
-			return (syntax_error(token_repr(curr->next)), 1);
-
-		if (is_redir(curr->type))
+		if (curr->type == TOKEN_WORD && curr->value)
 		{
-			if (!curr->next || curr->next->type == TOKEN_PIPE ||
-				is_redir(curr->next->type) || curr->next->type == TOKEN_EOF)
-				return (syntax_error(token_repr(curr->next)), 1);
+			if (!ft_strcmp(curr->value, "&&") || !ft_strcmp(curr->value, "||")
+				|| !ft_strcmp(curr->value, ";") || !ft_strcmp(curr->value, ";;")
+				|| !ft_strcmp(curr->value, "&") || !ft_strcmp(curr->value, "(")
+				|| !ft_strcmp(curr->value, ")"))
+				return (error_syntax(curr->value), 1);
 		}
-		if (curr->type == TOKEN_PIPE && curr->next && is_redir(curr->next->type))
-			return (syntax_error(token_repr(curr->next)), 1);
-
-		// PIPE à la fin
+		if (curr->type == TOKEN_PIPE && curr->next && curr->next->type == TOKEN_PIPE)
+			return (error_syntax("|"), 1);
 		if (curr->type == TOKEN_PIPE && (!curr->next || curr->next->type == TOKEN_EOF))
-			return (syntax_error("newline"), 1);
-
+			return (error_syntax("newline"), 1);
+		if (is_redir(curr->type) && curr->next && is_redir(curr->next->type))
+			return (error_syntax(token_repr(curr->next)), 1);
+		if (is_redir(curr->type) &&
+			(!curr->next || curr->next->type == TOKEN_PIPE
+				|| curr->next->type == TOKEN_EOF))
+			return (error_syntax(token_repr(curr->next)), 1);
 		curr = curr->next;
 	}
 	return (0);
