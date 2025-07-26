a039e9897728b3ef4e2407e4a8c14513d5517112
khalid058r <khalidabouelfaraj371@gmail.com>
2025-07-26 20:39:13 +0100

    done expend in herdoc

--- parsing/expension2/expend_herdoc.c
+++ parsing/expension2/expend_herdoc.c
@@ -1,10 +1,24 @@
 # include "../../minishell.h"
 
 
+// char *expand_heredoc_content(const char *str, t_env **env, int last_status, const char *delimiter)
+// {
+//     if (strchr(delimiter, '\'') || strchr(delimiter, '"'))  // PATCH: quoted delimiter
+//         return ft_strdup(str);
+//     (void) last_status;
+//     return expand_variables_in_word((char *)str, *env);
+// }
+
+
 char *expand_heredoc_content(const char *str, t_env **env, int last_status, const char *delimiter)
 {
-    if (strchr(delimiter, '\'') || strchr(delimiter, '"'))  // PATCH: quoted delimiter
+    t_token *token;
+    char *expended;
+     (void) last_status;
+    if (strchr(delimiter, '\'') || strchr(delimiter, '"'))
         return ft_strdup(str);
-    (void) last_status;
-    return expand_variables_in_word((char *)str, *env);
+    token = lst_new_token(str, TOKEN_WORD, false, 0); // attention à la signature de lst_new_token
+    expansion_all_tokens(token, *env);
+    expended = ft_strdup(token->value);
+    return expended;
 }
